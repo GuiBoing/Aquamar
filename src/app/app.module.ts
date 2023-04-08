86873244e8728b933fb35f619b9193e530513a52
GuiBoing <guiliboing@gmail.com>
2023-04-08 15:18:09 -0300

    criação de componentes de home, produtos,detalhamento produto,carrinho,checkout,pedidos,ajuda,login,cadastro

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ProdutosComponent } from './produtos/produtos.component';
+import { ProdutoComponent } from './produto/produto.component';
+import { CarrinhoComponent } from './carrinho/carrinho.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { PedidosComponent } from './pedidos/pedidos.component';
+import { AjudaComponent } from './ajuda/ajuda.component';
+import { LoginComponent } from './login/login.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProdutosComponent,
+    ProdutoComponent,
+    CarrinhoComponent,
+    CheckoutComponent,
+    PedidosComponent,
+    AjudaComponent,
+    LoginComponent,
+    CadastroComponent
   ],
   imports: [
     BrowserModule,
